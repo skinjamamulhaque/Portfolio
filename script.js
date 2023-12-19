668c223a09d9b9d2d214d60758939de00eba90cf
SK Injamamul Haque <V-SP0274@pnb.net>
2023-12-19 16:59:35 +0530

    responsiveness added

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 alert("This page is being developed by Sk Injamamul Haque, Best view on Desktop mode");
 
+function locoScrollTrigger(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// --- RED PANEL ---
+gsap.from(".line-1", {
+  scrollTrigger: {
+    trigger: ".line-1",
+    scroller: "#main",
+    // scrub: true,
+    start: "top bottom",
+    end: "top top",
+    onUpdate: self => console.log(self.direction)
+  },
+  scaleX: 0,
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locoScrollTrigger();
+
 // import Swal from 'sweetalert2/dist/sweetalert2.js'
 // import 'sweetalert2/src/sweetalert2.scss'
 
@@ -56,7 +106,8 @@ gsap.from(".content_details h2, .about_para, .Readmore",{
   duration:1.5,
   scrollTrigger:{
     trigger:".card_container .card",
-    scroller:"body",
+    scroller:"#main",
+    // scrub:2,
     // markers:true,
 
   }
@@ -69,7 +120,8 @@ gsap.from(".content_details h2, .about_para, .Readmore",{
   opacity:0,
   scrollTrigger:{
     trigger:".content_details h3",
-    scroller:"body",
+    scroller:"#main",
+    // scrub:1,
   }
  })
 
@@ -79,7 +131,8 @@ gsap.from(".content_details h2, .about_para, .Readmore",{
   opacity:0,
   scrollTrigger:{
     trigger:".content_box h3",
-    scroller:"body",
+    scroller:"#main",
+    // scrub:2,
   }
  })
 
@@ -88,7 +141,8 @@ gsap.from(".content_details h2, .about_para, .Readmore",{
   duration:2,
   scrollTrigger:{
     trigger:".card1 img",
-    scroller:"body",
+    scroller:"#main",
+    // scrub:2,
   }
 })
 
