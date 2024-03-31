749c5e429c7d56cc2695f0f3d927bcdcc78693a8
Sk Injamamul Haque <injamamulhaque76@gmail.com>
2024-03-31 21:45:22 +0530

    bug fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //display hidden feature for About Page Details Button
-document.querySelector("#getDetaiBtn").addEventListener('click',function(e){
+document.querySelector("#getDetailBtn").addEventListener('click',function(e){
   document.querySelector("#invisibleBtn").classList.remove('md:hidden', 'hidden')
 })
